3f7e11ed0bf6c6f606f6e72ef28592e7481cc134
dennis ko <dennis0324@naver.com>
2023-06-17 02:34:13 +0900

    change: lib from personal_tray to capunction_tray

--- src/main/main.c
+++ src/main/main.c
@@ -1,23 +1,21 @@
-#include <uiohook.h>
+// #include <uiohook.h>
+#include <stdio.h>
+#include <string.h>
 #include <tray.h>
 #include <hook.h>
-#include <personal_tray.h>
+#include <capunction_tray.h>
+
 #include <stdio.h>
 
-
 int main() {
-
+    // #ifdef 
     // Set the logger callback for library output.
     hook_set_logger_proc(&logger_proc);
     
     // Set the event callback for uiohook events.
     hook_set_dispatch_proc(&dispatch_proc);
 
-    // Start the hook and block.
-    // NOTE If EVENT_HOOK_ENABLED was delivered, the status will always succeed.
-    // int status = create_tray();
     
     personalwhile();
-
     return 0;
 }
